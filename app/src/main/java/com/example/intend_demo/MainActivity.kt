a8d473c9a4fbf26806709f5819f26de7e5169f03
DEV <pattnaikd833@gmail.com>
2025-03-13 18:49:35 +0530

    Implement username and password validation on login page
    - Added validation for username and password fields on the login page.
    - Display a toast message "Invalid Credentials" if username or password does not match admin.
    - Launched the home page when username and password match admin.

--- app/src/main/java/com/example/intend_demo/MainActivity.kt
+++ app/src/main/java/com/example/intend_demo/MainActivity.kt
@@ -3,22 +3,30 @@ package com.example.intend_demo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val editTextUsername = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.editTextUsername)
+        val editTextPassword = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.editTextPassword)
 
-        val button = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.buttonLogin)
         button.setOnClickListener {
+            // Launch the HomePage activity
+            // Replace HomePage::class.java with the actual name of your HomePage activity
+            if (editTextUsername.text.toString() == "admin" && editTextPassword.text.toString() == "admin") {
+
             val intent = Intent(this, HomePage::class.java)
             startActivity(intent)
         }
-
+            else{
+                Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+            }
     }
-}
+}}
